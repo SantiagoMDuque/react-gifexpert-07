1b22811971ded68590eb308af545903daece5f37
SantiagoMDuque <santiago.mduque94@gmail.com>
2024-01-19 18:02:48 -0500

    pruebas subidas para despliegue

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({onNewCategory}) => {
 
@@ -7,16 +8,13 @@ export const AddCategory = ({onNewCategory}) => {
     const onSubmit = (event) => {
         event.preventDefault(); 
         if(inputValue.trim().length <= 0) return;
-        // setCategories((categorie) => [
-        //     inputValue,
-        //     ...categorie,
-        // ])
+
         onNewCategory( inputValue.trim() )
         setInputValue('');
     }
 
     return(
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form">
             <input 
                 type="text" 
                 value={inputValue} 
@@ -26,4 +24,8 @@ export const AddCategory = ({onNewCategory}) => {
         </form>
        
     )
+}
+
+AddCategory.prototype = {
+    onNewCategory: PropTypes.func.isRequired
 }
---
+++ test/GifExpertApp.test.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react"
+import { GifExperApp } from "../src/GifExpertApp"
+
+describe('preuba de GifExpertApp', () => {
+    test('', () => {
+        render(<GifExperApp/>)
+        //Tarea por hacer 
+    })
+    
+})
